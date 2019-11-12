9b48cbfa005f246f2ff8420f1869ac839816ce33
saramarin23 <saramarin.econ@gmail.com>
2019-11-12 14:04:15 +0100

    the app loads the data

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log(">> Ready :)");
+// let btn = document.querySelector(".button");
+let title = document.querySelector(".recipe-title");
+const data = "../../service/data.json";
+
+function fetchData() {
+  fetch(data)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      title.innerHTML = data.name;
+    });
+}
+
+window.addEventListener("load", fetchData);
