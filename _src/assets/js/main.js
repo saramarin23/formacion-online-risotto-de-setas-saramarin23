fd32e76f5a56444f6a3fa2dcbb2d1c90cd7018b8
saramarin23 <saramarin.econ@gmail.com>
2019-11-17 19:32:43 +0100

    add selected checkboxes, and limit input quantity

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,16 +2,15 @@
 
 // app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // redirect CSS bootstrap
 
-// let btn = document.querySelector(".button");
 let title = document.querySelector(".recipe-title");
 const data = "../../service/data.json";
+let shipping = 7;
 
 function fetchData() {
   const shippingCost = document.querySelector(".shipping-costs");
   fetch(data)
     .then(response => response.json())
     .then(data => {
-      //   console.log(data);
       title.innerHTML = data.name;
       let products = document.querySelector(".products-list");
       let items = document.querySelector(".total-items");
@@ -22,30 +21,25 @@ function fetchData() {
 
       let ingredientItem = "";
       for (const ingredient of data.ingredients) {
-        // console.log(ingredient);
         ingredientItem += `<li class="list-group-item list-item">
-        <button class="form-check-input position-static" type="checkbox">
-        </button>
-        <input type="number" class="input-quantity quantity" value="${
+        <input class="form-check-input position-static" type="checkbox" checked>
+        </input>
+        <input type="number" class="input-quantity quantity" value=${
           ingredient.items
-        }"></input>
+        } min="1"></input>
         <div class="ingredient-container">
             <p class="product">${ingredient.product}</p>
-            <p class="product-brand">${ingredient.brand}</p>
+            <p class="product-brand">${ingredient.brand || "Sin marca"} </p>
             <span class="product-quantity">${ingredient.quantity}</span>
         </div>
         <p class="product-price">${ingredient.price *
           ingredient.items} €</p></li>`;
-        {
-          console.log(ingredient.price * ingredient.items);
-        }
       }
       products.innerHTML = ingredientItem;
       //   subtotal.innerHTML;
-      shippingCost.innerHTML += `<p> 7,00 ${data.currency}</p>`;
-      console.log(shippingCost.lastElementChild.innerHTML);
-      total.innerHTML += `<p>${shippingCost.lastElementChild.innerHTML}</p>`;
-      buy.innerHTML += total.lastElementChild.innerHTML;
+      shippingCost.innerHTML += `<p> ${shipping} ${data.currency}</p>`;
+      // total.innerHTML += `<p>${shippingCost.lastElementChild.innerHTML}</p>`;
+      buy.innerHTML += shipping + data.currency;
     });
 }
 
